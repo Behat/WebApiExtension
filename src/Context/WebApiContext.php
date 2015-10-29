dfe76062c034e7e188e4ba894916cfae1e728f93
Tobias Happ <tobias.happ@gmx.de>
2015-10-29 01:11:44 +0100

    Changed accessibility of properties to protected

--- src/Context/WebApiContext.php
+++ src/Context/WebApiContext.php
@@ -26,29 +26,29 @@ class WebApiContext implements ApiClientAwareContext
     /**
      * @var string
      */
-    private $authorization;
+    protected $authorization;
 
     /**
      * @var ClientInterface
      */
-    private $client;
+    protected $client;
 
     /**
      * @var array
      */
-    private $headers = array();
+    protected $headers = array();
 
     /**
      * @var \GuzzleHttp\Message\RequestInterface
      */
-    private $request;
+    protected $request;
 
     /**
      * @var \GuzzleHttp\Message\ResponseInterface
      */
-    private $response;
+    protected $response;
 
-    private $placeHolders = array();
+    protected $placeHolders = array();
 
     /**
      * {@inheritdoc}
