7f6098dcfbd6abeed2dab17d2f867713154b5d09
Anton Vorobyev <anton@vorobyev.co>
2016-06-23 14:52:20 +0500

    Fix content type detection in test app

--- testapp/index.php
+++ testapp/index.php
@@ -29,7 +29,7 @@ $app->match(
         $content = $req->getContent(false);
         if (!empty($content)) {
 
-            if ('application/json' == $req->getContentType()) {
+            if (0 === strpos($req->headers->get('Content-Type'), 'application/json')) {
 
                 $data = json_decode($content, true);
                 if (!is_array($data)) {
