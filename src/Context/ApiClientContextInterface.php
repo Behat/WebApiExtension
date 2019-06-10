9b21a779b15c8e1a7314d2d6f09207dd6899bb79
Kevin Lot <klot@keyclic.com>
2019-06-10 17:39:17 +0200

    Add methods to respect contracts of interface.
    Remove deprecated.
    Add return types.

--- src/Context/ApiClientContextInterface.php
+++ src/Context/ApiClientContextInterface.php
@@ -12,21 +12,25 @@
 namespace Behat\WebApiExtension\Context;
 
 use Behat\Behat\Context\Context;
-use GuzzleHttp\ClientInterface;
+use Psr\Http\Client\ClientInterface;
 
 /**
  * Guzzle Client-aware interface for contexts.
  *
  * @author Frédéric G. Marand <fgm@osinet.fr>
+ * @author Keyclic team <techies@keyclic.com>
  *
  * @see WebApiAwareInitializer
  */
 interface ApiClientContextInterface extends Context
 {
     /**
-     * Sets Guzzle Client instance.
-     *
-     * @param ClientInterface $client guzzle Client instance
+     * Sets HttpClient instance.
      */
-    public function setClient(ClientInterface $client);
+    public function setClient(ClientInterface $client): self;
+
+    /**
+     * Sets base of uri string.
+     */
+    public function setBaseUri(string $uri): self;
 }
